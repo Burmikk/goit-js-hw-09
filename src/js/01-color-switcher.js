4f4ebb0b362ac1634f1319af6a49517c250ab87a
Burmikk <byrmik@gmail.com>
2023-01-16 17:13:06 +0200

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startEl = document.querySelector("[data-start]")
+const stopEl = document.querySelector("[data-stop]")
+const bodyEl = document.querySelector("body")
+let intervalId = null
+
+function onChangeBgColor() {
+    startEl.setAttribute("disabled", "")
+
+    intervalId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor()
+    }, 1000)
+}
+startEl.addEventListener("click", onChangeBgColor)
+
+
+function onStopChangeBgColor() {
+    startEl.removeAttribute("disabled")
+    clearInterval(intervalId)
+}
+stopEl.addEventListener ("click", onStopChangeBgColor)
