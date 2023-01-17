3f2e64ca11587f4904cb8c603580795bb802b566
Burmikk <byrmik@gmail.com>
2023-01-17 02:11:57 +0200

    task-2 add styles

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,7 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import Notiflix from "notiflix";
+require("flatpickr/dist/themes/dark.css");
 
 
 const inputEl = document.querySelector("#datetime-picker")
@@ -16,45 +17,68 @@ let changeTimeInterval
 
 btnStartEl.setAttribute("disabled", "")
 
-// Вызов библиотеки чтоб в инпуте появился календарь
-flatpickr(inputEl, options)
-
 // Опции которые передаются в кадендарь
 const options = {
+  
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-      if (selectedDates[0] < options.defaultDate) {
-         return Notiflix.Notify.failure("Please choose a date in the future");
+      if (selectedDates[0] <= new Date()) {
+          
+        btnStartEl.setAttribute("disabled", "")
+        btnStartEl.classList.remove("activ-green")
 
-      } 
-            btnStartEl.removeAttribute("disabled")
+          return Notiflix.Notify.failure("Please choose a date in the future");
+          
+
+      } else {
+          btnStartEl.removeAttribute("disabled")
+        btnStartEl.classList.add("activ-green")
+      
             futureTime = selectedDates[0]
+      }
+      
       
   },
 };
 
+// Вызов библиотеки чтоб в инпуте появился календарь
+flatpickr(inputEl, options)
+
 function onCountTime() {
     deltaTime = futureTime - new Date()
     const { days, hours, minutes, seconds } = convertMs(deltaTime)
-
+    
     secondsEl.textContent = addLeadingZero(seconds)
     minutesEl.textContent = addLeadingZero(minutes)
     hoursEl.textContent = addLeadingZero(hours)
     daysEl.textContent = addLeadingZero(days)
     if (days === 0  && hours === 0 && minutes === 0 && seconds === 0) {
         clearInterval(changeTimeInterval)
+       
+        changeStyleIfClickStop()
+
+
         
     }
 }
 
 btnStartEl.addEventListener("click", () => {
+    if (futureTime <= new Date) {
+        btnStartEl.classList.remove("activ-green")
+        btnStartEl.setAttribute("disabled", "")
+        return Notiflix.Notify.failure("Please choose a date in the future");
+    }
+
     changeTimeInterval = setInterval(onCountTime, 1000)
 
+    inputEl.setAttribute("disabled", "")
     btnStartEl.setAttribute("disabled", "")
     btnStopEl.removeAttribute("disabled")
+    btnStartEl.classList.remove("activ-green")
+    btnStopEl.classList.add("activ-red")
 })
 
 
@@ -67,7 +91,8 @@ btnStopEl.addEventListener("click", () => {
     minutesEl.textContent = "00"
     hoursEl.textContent = "00"
     daysEl.textContent = "00"
-    btnStopEl.setAttribute("disabled", "")
+    
+    changeStyleIfClickStop()
 
 })
 
@@ -95,4 +120,10 @@ function convertMs(ms) {
 function addLeadingZero(value) {
     return String(value).padStart(2, "0")
 
+}
+
+function changeStyleIfClickStop() {
+    btnStopEl.setAttribute("disabled", "")
+    btnStopEl.classList.remove("activ-red")
+     inputEl.removeAttribute("disabled")
 }
