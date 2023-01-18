cc5ea5f30d929082386799f0624df493616670a5
Burmikk <byrmik@gmail.com>
2023-01-18 17:13:21 +0200

    task-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,48 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+
+import Notiflix from "notiflix";
+
+
+const formEl = document.querySelector(".form")
+const buttonEl = document.querySelector("button")
+const delayEl = document.querySelector("delay")
+const amountEl = document.querySelector("amount")
+
+
+formEl.addEventListener("submit", startCreatePromise)
+
+function startCreatePromise(e) {
+  e.preventDefault()
+  const delay = +e.target.elements.delay.value
+  const amount = +e.target.elements.amount.value
+  const step = +e.target.elements.step.value
+  for (i = 0; i < amount; i++){
+    
+  createPromise(i+1, delay + step * i)
+    .then(data => Notiflix.Notify.success(`Fulfilled promise ${data.position} in ${data.delay} `))
+    .catch(data => Notiflix.Notify.failure(`Rejected promise ${data.position} in ${data.delay} `))
+    // delay+=step
+
   }
+  
 }
+
+function createPromise(position, delay) {
+
+  const shouldResolve = Math.random() > 0.3;
+
+
+     
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+      resolve({position, delay})
+    } else {
+      reject({position, delay})
+    }
+    },delay)
+    
+    
+
+  })
+    }
+    
